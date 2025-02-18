664ffdabed763b31bd79db950b9df736baa84e98
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-02-18 10:38:55 +0000

    Модули и импорты
    - Примеры конструкции: `if __name__ == '__main__':`

---
+++ app.py
@@ -0,0 +1,9 @@
+from file_18_02 import say_hello
+
+
+def start_page():
+    print(say_hello("Alice"))
+
+
+if __name__ == '__main__':
+    start_page()
---
+++ file_18_02.py
@@ -0,0 +1,9 @@
+def say_hello(name: str) -> str:
+    return f"Привет, {name}!"
+
+
+# print(__name__)
+
+if __name__ == '__main__':
+    print(say_hello(input("Введите тествоое имя: ")))
+    print(2 + 3)
