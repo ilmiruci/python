e11b91efc46657f7a96f5de944552a4069413a53
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-02-18 10:49:21 +0000

    Возвращаемое значение функции
    - Аннотация функции

--- file_18_02.py
+++ file_18_02.py
@@ -2,8 +2,17 @@ def say_hello(name: str) -> str:
     return f"Привет, {name}!"
 
 
+def example() -> None:
+    print("Hi!")
+
+    return
+    # return None
+
+
 # print(__name__)
 
+
 if __name__ == '__main__':
-    print(say_hello(input("Введите тествоое имя: ")))
-    print(2 + 3)
+    print(example())
+    # print(say_hello(input("Введите тествоое имя: ")))
+    # print(2 + 3)
